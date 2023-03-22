ca9a696c6b64c1f628e6519140bd0a342b561c29
poojitharaj1 <tpoojitharaj@gmail.com>
2023-03-22 22:03:31 +1100

    Initial commit

---
+++ windows/mymovies/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::mymovies::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::mymovies::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
